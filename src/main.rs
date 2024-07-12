a441097889ccf58562631cdf7a978adae7b82917
Yash Karandikar <nerdstep710@gmail.com>
2024-07-12 07:39:42 +0530

    Simple GTK hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow, Label};
+
+const APP_ID: &str = "xyz.karx.CRABTAGGER";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let label = Label::new(Some("Hello, world!"));
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Crabtagger")
+        .child(&label)
+        .build();
+
+    window.present();
 }
